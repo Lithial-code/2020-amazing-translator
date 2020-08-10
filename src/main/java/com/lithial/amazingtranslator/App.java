9cae025ddad631c215024ceee630610a2eed6ffb
Lithial <jameshessell@gmail.com>
2020-08-10 14:58:58 +1200

    code:Initial commit

---
+++ src/main/java/com/lithial/amazingtranslator/App.java
@@ -0,0 +1,14 @@
+package com.lithial.amazingtranslator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //This is a test
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/lithial/amazingtranslator/AppTest.java
@@ -0,0 +1,20 @@
+package com.lithial.amazingtranslator;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
